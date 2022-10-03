b25d613529aa87f2800762147f3058040cc32c99
Daniel Cazé <danielctlima@gmail.com>
2022-10-03 10:37:11 -0300

    themes with styled components

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/Button";
+
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button />
+      <Button variant={"primary"} />
+      <Button variant={"success"} />
+      <Button variant={"warning"} />
+      <Button variant={"danger"} />
+      <Button variant={"secondary"} />
+    </ThemeProvider>
+  )
+}
---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { ButtonContainer } from "./Button.styles"
+
+export type ButtonVariant = "primary" | "secondary" | "success" | "warning" | "danger"
+
+interface ButtonProps {
+    variant?: ButtonVariant
+}
+
+export function Button({ variant = "primary" }: ButtonProps) {
+    return (
+        <ButtonContainer variant={variant}>Button</ButtonContainer>
+    )
+}
